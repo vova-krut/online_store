5946b4dbb245370d0551730d2a83ac72db2ad2c8
Volodymyr <vthehang@gmail.com>
2022-06-01 17:45:24 +0300

    added errorHandler

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,17 @@
+import ApiError from "../error/ApiError.js";
+
 class UserController {
     async registration(req, res) {}
 
     async login(req, res) {}
 
-    async check(req, res) {}
+    async check(req, res, next) {
+        const { id } = req.query;
+        if (!id) {
+            return next(ApiError.badRequest("Id is not set"));
+        }
+        res.json(id);
+    }
 }
 
 export default new UserController();
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import sequalize from "./db.js";
 import cors from "cors";
 import router from "./routes/index.js";
+import errorHandler from "./middleware/ErrorHandlingMiddleware.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
+app.use(errorHandler);
 
 const start = async () => {
     try {
