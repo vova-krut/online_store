c20dc6d18464bb54ccacbf073fb81c649604eec3
Volodymyr <vthehang@gmail.com>
2022-06-02 15:24:43 +0300

    added registration and login functions in userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,46 @@
 import ApiError from "../error/ApiError.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { User, Basket } from "../models/models.js";
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+        expiresIn: "24h",
+    });
+};
 
 class UserController {
-    async registration(req, res) {}
+    async registration(req, res, next) {
+        const { email, password, role } = req.body;
+        if (!email || !password) {
+            return next(ApiError.badRequest("Email or password is incorrect"));
+        }
+        const candidate = await User.findOne({ where: { email } });
+        if (candidate) {
+            return next(
+                ApiError.badRequest("User with this email already exists")
+            );
+        }
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({ email, role, password: hashPassword });
+        const basket = await Basket.create({ userId: user.id });
+        const token = generateJwt(user.id, user.email, user.role);
+        return res.json({ token });
+    }
 
-    async login(req, res) {}
+    async login(req, res, next) {
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+            return next(ApiError.internal("User with this email is not found"));
+        }
+        const comparePasswords = bcrypt.compareSync(password, user.password);
+        if (!comparePasswords) {
+            return next(ApiError.internal("You have entered a wrong password"));
+        }
+        const token = generateJwt(user.id, user.email, user.role);
+        return res.json({ token });
+    }
 
     async check(req, res, next) {
         const { id } = req.query;
