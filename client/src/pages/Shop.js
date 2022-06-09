45059ab9731606f3d03f32144651daff67821907
Volodymyr <vthehang@gmail.com>
2022-06-09 17:54:23 +0300

    added deviceGetting from server

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import TypeBar from "../components/TypeBar";
 import BrandBar from "../components/BrandBar";
 import DeviceList from "../components/DeviceList";
+import { observer } from "mobx-react-lite";
+import { Context } from "../index";
+import { fetchBrands, fetchDevices, fetchTypes } from "../http/deviceAPI";
 
-const Shop = () => {
+const Shop = observer(() => {
+    const { device } = useContext(Context);
+
+    useEffect(() => {
+        fetchTypes().then((data) => device.setTypes(data));
+        fetchBrands().then((data) => device.setBrands(data));
+        fetchDevices().then((data) => device.setDevices(data.rows));
+    }, [device]);
+
     return (
         <Container>
             <Row className="mt-2">
@@ -20,6 +31,6 @@ const Shop = () => {
             </Row>
         </Container>
     );
-};
+});
 
 export default Shop;
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -7,76 +7,9 @@ export default class DeviceStore {
     _selectedBrand;
     _devices;
     constructor() {
-        this._types = [
-            { id: 1, name: "Fridges" },
-            { id: 2, name: "Smartphones" },
-            { id: 3, name: "Laptops" },
-            { id: 4, name: "TVs" },
-        ];
-        this._brands = [
-            { id: 1, name: "Samsung" },
-            { id: 2, name: "Apple" },
-            { id: 3, name: "Lenovo" },
-            { id: 4, name: "Asus" },
-        ];
-        this._devices = [
-            {
-                id: 1,
-                name: "Iphone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: "https://istudio.ua/upload/iblock/59c/iphone12-pro-128-gb-pacific-blue.png",
-            },
-            {
-                id: 2,
-                name: "Iphone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: "https://istudio.ua/upload/iblock/59c/iphone12-pro-128-gb-pacific-blue.png",
-            },
-            {
-                id: 3,
-                name: "Iphone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: "https://istudio.ua/upload/iblock/59c/iphone12-pro-128-gb-pacific-blue.png",
-            },
-            {
-                id: 4,
-                name: "Iphone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: "https://istudio.ua/upload/iblock/59c/iphone12-pro-128-gb-pacific-blue.png",
-            },
-            {
-                id: 5,
-                name: "Iphone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: "https://istudio.ua/upload/iblock/59c/iphone12-pro-128-gb-pacific-blue.png",
-            },
-            {
-                id: 6,
-                name: "Iphone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: "https://istudio.ua/upload/iblock/59c/iphone12-pro-128-gb-pacific-blue.png",
-            },
-            {
-                id: 7,
-                name: "Iphone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: "https://istudio.ua/upload/iblock/59c/iphone12-pro-128-gb-pacific-blue.png",
-            },
-            {
-                id: 8,
-                name: "Iphone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: "https://istudio.ua/upload/iblock/59c/iphone12-pro-128-gb-pacific-blue.png",
-            },
-        ];
+        this._types = [];
+        this._brands = [];
+        this._devices = [];
         this._selectedType = {};
         this._selectedBrand = {};
         makeAutoObservable(this);
